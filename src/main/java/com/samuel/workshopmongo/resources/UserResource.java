6634231495ad59493989d8109b322ec4d4013848
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-26 23:24:40 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/samuel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/samuel/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.samuel.workshopmongo.domain.Post;
 import com.samuel.workshopmongo.domain.User;
 import com.samuel.workshopmongo.dto.UserDTO;
 import com.samuel.workshopmongo.services.UserService;
@@ -70,5 +71,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+
+	}
+	
 
 }
