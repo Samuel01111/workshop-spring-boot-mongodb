4a1dc76cb8a3afab2d25b127d8edd8e3dc743d24
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-27 11:04:13 -0300

    Consulta simples com @Query

--- src/main/java/com/samuel/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/samuel/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.samuel.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.samuel.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.samuel.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	//{ <field>: { $regex: /pattern/, $options: '<options>' } }
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String txt);
 }
