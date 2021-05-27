d5654ff91ae021f54259752ca8e49c51e663832a
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-27 12:07:42 -0300

    Consulta com varios criterios

--- src/main/java/com/samuel/workshopmongo/services/PostService.java
+++ src/main/java/com/samuel/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.samuel.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 
 	public List<Post> findByTitle(String txt) {
 		return repo.searchTitle(txt);
+	}
+	// tem que adicionar um dia amais, por que tem que ser até o final do proximo dia, porcausa do milisegundos;
+	public List<Post> fullSeach(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
