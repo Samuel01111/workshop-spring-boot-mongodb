67a61af875d8818625507ed1cee4299f3a51d9d4
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-26 23:16:51 -0300

    Referenciando os posts do usuário

--- src/main/java/com/samuel/workshopmongo/domain/User.java
+++ src/main/java/com/samuel/workshopmongo/domain/User.java
@@ -1,19 +1,25 @@
 package com.samuel.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -47,6 +53,10 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -71,6 +81,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
